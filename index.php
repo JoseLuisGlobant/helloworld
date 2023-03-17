e7b4ed74a62e6e96dade00e00c17a4395b962909
Jose Alfredo Luis Medina <jose.luis@clearesult.com>
2023-03-16 18:46:00 -0600

    mongo class

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $mongoUrl = "mongodb+srv://mo0ng0us3r:bm5BTptW8RD9tpBa@globant.qgp6k40.mongodb.net/test?retryWrites=true&w=majority";
-$client = new MongoDB\Client($mongoUrl);
+$client = new \MongoDB\Client($mongoUrl);
 
 $collection = $client->test->messages;
 
