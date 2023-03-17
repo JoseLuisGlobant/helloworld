63f3170dee7f217098eb949e1625e02f136fc0c6
Jose Alfredo Luis Medina <jose.luis@clearesult.com>
2023-03-16 21:03:37 -0600

    php info

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 
-$mongoUrl = "mongodb+srv://mo0ng0us3r:bm5BTptW8RD9tpBa@globant.qgp6k40.mongodb.net/test?retryWrites=true&w=majority";
-$client = new \MongoDB\Client($mongoUrl);
-
-$collection = $client->test->messages;
-
-$result = $collection->find();
-foreach ($result as $document) {
-    echo $document['message'] . "\n";
-}
-
-
+phpinfo();
